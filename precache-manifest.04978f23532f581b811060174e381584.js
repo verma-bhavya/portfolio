1900755252f6ad853a89901c8080d6b7a1765a4d
Bhavya Verma <bhavyaverma1699@gmail.com>
2020-05-02 23:23:09 +0530

    Updates

---
+++ precache-manifest.04978f23532f581b811060174e381584.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8964d02de8b9c09f1d8b1f27c7d97ff7",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "ce0cbe9b0a9e3a6ee36e",
+    "url": "/portfolio/static/css/main.431ad087.chunk.css"
+  },
+  {
+    "revision": "afd3acef43b10074288f",
+    "url": "/portfolio/static/js/2.6ce4f067.chunk.js"
+  },
+  {
+    "revision": "0f544458a968df1596e861027de46f92",
+    "url": "/portfolio/static/js/2.6ce4f067.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce0cbe9b0a9e3a6ee36e",
+    "url": "/portfolio/static/js/main.3f4fa21f.chunk.js"
+  },
+  {
+    "revision": "e5284505c87d0b24b753",
+    "url": "/portfolio/static/js/runtime-main.f4f2abbf.js"
+  }
+]);
